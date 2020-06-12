935133da856de80c73144db47361e815fd7d8a86
ZhuLei71 <51832496+ZhuLei71@users.noreply.github.com>
2020-06-12 10:34:10 -0600

    Changed order in equivalent comparing expression

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,21 +11,15 @@ class Quiz extends Component {
     }
 
     render() {
-        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 1;
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
         
-        if (isQuizEnd) {
-            return (
-                <div>
-                    <QuizEnd />
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-                </div>
-            );
-        }
+        return(
+            <div>
+            {isQuizEnd ?  <QuizEnd /> : 
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+            }
+            </div>        
+        );
     } 
 }
 
