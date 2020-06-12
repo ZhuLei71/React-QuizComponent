13b983255a18f120093743850da6b609313c5680
ZhuLei71 <51832496+ZhuLei71@users.noreply.github.com>
2020-06-11 21:20:57 -0600

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,10 +3,13 @@ import React, { component } from 'react';
 let quizData = require('./quiz_data.json');
 
 class Quiz extends React.Component {
-    render = () => 
-        <div>
-            Quiz
-        </div>
+    render() {
+        Return (
+            <div>
+                Quiz
+            </div>
+        );
+    } 
 }
 
 export default Quiz;
